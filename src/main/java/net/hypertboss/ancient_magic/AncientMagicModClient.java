edc5f51106a0b1426d543ed9150a77a0879ebab5
AkeemOB <sonion13@gmail.com>
2022-12-07 18:26:56 +0000

    Init Project

---
+++ src/main/java/net/hypertboss/ancient_magic/AncientMagicModClient.java
@@ -0,0 +1,10 @@
+package net.hypertboss.ancient_magic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientMagicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/net/hypertboss/ancient_magic/mixin/ExampleMixin.java
+++ src/main/java/net/hypertboss/ancient_magic/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.hypertboss.ancient_magic.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.hypertboss.ancient_magic.Ancient_Magic;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Ancient_Magic.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
